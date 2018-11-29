34263f0ad8321610dd21515b9e265b579a883ebb
Dimitri KAHN <dimitri.kahn@bforbank.com>
2018-11-29 16:35:33 +0100

    impl step 6

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
-import { LoginModalService, Principal, Account } from 'app/core';
+import { Account, LoginModalService, Principal } from 'app/core';
+import { TicketService } from 'app/entities/ticket';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { ITicket } from 'app/shared/model/ticket.model';
 
 @Component({
     selector: 'jhi-home',
@@ -13,11 +16,21 @@ export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
 
-    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
+    myTickets: ITicket[];
+
+    constructor(
+        private principal: Principal,
+        private ticketService: TicketService,
+        private loginModalService: LoginModalService,
+        private eventManager: JhiEventManager
+    ) {}
 
     ngOnInit() {
         this.principal.identity().then(account => {
             this.account = account;
+            if (this.account) {
+                this.loadMyTickets();
+            }
         });
         this.registerAuthenticationSuccess();
     }
@@ -26,8 +39,17 @@ export class HomeComponent implements OnInit {
         this.eventManager.subscribe('authenticationSuccess', message => {
             this.principal.identity().then(account => {
                 this.account = account;
+                if (this.account) {
+                    this.loadMyTickets();
+                }
             });
         });
+    }
+
+    loadMyTickets() {
+        this.ticketService
+            .findMine()
+            .subscribe((res: HttpResponse<ITicket[]>) => (this.myTickets = res.body), (res: HttpErrorResponse) => alert(res.message));
     }
 
     isAuthenticated() {
